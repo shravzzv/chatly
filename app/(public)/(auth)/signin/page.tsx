c3e007ee704bf09fc8b440059456a00433742170
Sai Shravan <saishravan384@gmail.com>
2025-12-01 20:56:57 +0530

    chore: add Suspense in signinform and signupform pages

--- app/(public)/(auth)/signin/page.tsx
+++ app/(public)/(auth)/signin/page.tsx
@@ -1,12 +1,13 @@
-'use client'
-
 import { SigninForm } from '@/components/signin-form'
+import { Suspense } from 'react'
 
 export default function Page() {
   return (
     <div className='bg-muted flex min-h-svh flex-col items-center justify-center p-4 md:p-8'>
       <div className='w-full max-w-sm md:max-w-4xl'>
-        <SigninForm />
+        <Suspense>
+          <SigninForm />
+        </Suspense>
       </div>
     </div>
   )
