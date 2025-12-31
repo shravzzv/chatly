fea202b31b0f5a1f0997b7a80028eebc5ec34e50
Sai Shravan <saishravan384@gmail.com>
2025-12-31 10:42:39 +0530

    chore: add jest and playwright configs to eslint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,10 +1,26 @@
 import { defineConfig, globalIgnores } from 'eslint/config'
 import nextVitals from 'eslint-config-next/core-web-vitals'
 import nextTs from 'eslint-config-next/typescript'
+import pluginJest from 'eslint-plugin-jest'
+import pluginPlaywright from 'eslint-plugin-playwright'
 
 const eslintConfig = defineConfig([
   ...nextVitals,
   ...nextTs,
+
+  {
+    files: ['**/*.test.{ts,tsx}', '**/*.spec.{ts,tsx}'],
+    plugins: { jest: pluginJest },
+    rules: {
+      ...pluginJest.configs.recommended.rules,
+    },
+  },
+
+  {
+    files: ['e2e/**/*.{ts,tsx}'],
+    extends: [pluginPlaywright.configs['flat/recommended']],
+  },
+
   // Override default ignores of eslint-config-next.
   globalIgnores([
     // Default ignores of eslint-config-next:
@@ -15,6 +31,9 @@ const eslintConfig = defineConfig([
 
     // Generated UI components (shadcn/ui)
     'components/ui/**',
+
+    // ignore jest coverage
+    'coverage/**',
   ]),
 ])
 
