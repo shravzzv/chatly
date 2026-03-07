822764f837c1ad4ad651128bdd6b839c79d09139
Sai Shravan <saishravan384@gmail.com>
2026-03-07 08:54:33 +0530

    chore(native): setup nativewind

--- apps/native/app/index.tsx
+++ apps/native/app/index.tsx
@@ -1,10 +1,9 @@
-// apps/native/app/index.tsx
 import { Text, View } from 'react-native'
 
 export default function Index() {
   return (
     <View>
-      <Text>Hi from Chatly</Text>
+      <Text className='text-blue-500 underline font-black text-center'>Hi from Chatly</Text>
     </View>
   )
 }
