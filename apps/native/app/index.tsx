209ea7f64de2bbe69bdfc31527655aef4753bb2d
Sai Shravan Vadla <94429984+shravzzv@users.noreply.github.com>
2026-03-09 15:50:30 +0530

    chore(native): setup development guardrails (#74)
    * chore(deps): install natiwind with tailwindcss
    * chore(native): setup nativewind
    * fix(repo): reformat after CI error
    * chore(native): configure jest and testing library
    * fix(web): update `jest.config.ts` to remove arg errors
    * chore(web): exclude jest config in tsconfig
    * chore(native): setup maestro for e2e testing
    * chore(ci): setup jest workflow for native app
    * chore(native): setup eas with  prod. and preview scripts
    * feat(native): align `app.json` icons with Chatly's identity
    * docs: update `TESTING.md`
    * fix(format): format `TESTING.md` after CI error

--- apps/native/app/index.tsx
+++ apps/native/app/index.tsx
@@ -1,10 +1,11 @@
-// apps/native/app/index.tsx
 import { Text, View } from 'react-native'
 
 export default function Index() {
   return (
     <View>
-      <Text>Hi from Chatly</Text>
+      <Text className='text-center font-black text-blue-500 underline'>
+        Hi from Chatly
+      </Text>
     </View>
   )
 }
