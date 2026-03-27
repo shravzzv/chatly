792f673bdcf36a3a969eeb28aed9862ef1b3495a
Sai Shravan <saishravan384@gmail.com>
2026-03-27 21:02:41 +0530

    fix: update formatting in installed RNR component

--- apps/native/components/ui/native-only-animated-view.tsx
+++ apps/native/components/ui/native-only-animated-view.tsx
@@ -1,5 +1,5 @@
-import { Platform } from 'react-native';
-import Animated from 'react-native-reanimated';
+import { Platform } from 'react-native'
+import Animated from 'react-native-reanimated'
 
 /**
  * This component is used to wrap animated views that should only be animated on native.
@@ -11,13 +11,14 @@ import Animated from 'react-native-reanimated';
  * </NativeOnlyAnimatedView>
  */
 function NativeOnlyAnimatedView(
-  props: React.ComponentProps<typeof Animated.View> & React.RefAttributes<Animated.View>
+  props: React.ComponentProps<typeof Animated.View> &
+    React.RefAttributes<Animated.View>,
 ) {
   if (Platform.OS === 'web') {
-    return <>{props.children as React.ReactNode}</>;
+    return <>{props.children as React.ReactNode}</>
   } else {
-    return <Animated.View {...props} />;
+    return <Animated.View {...props} />
   }
 }
 
-export { NativeOnlyAnimatedView };
+export { NativeOnlyAnimatedView }
