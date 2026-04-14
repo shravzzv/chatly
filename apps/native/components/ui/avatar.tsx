39787ff75175bc7968667e3ce14c7182b6586e52
Sai Shravan <saishravan384@gmail.com>
2026-04-14 11:21:31 +0530

    fix(native): format new avatar ui component

--- apps/native/components/ui/avatar.tsx
+++ apps/native/components/ui/avatar.tsx
@@ -1,5 +1,5 @@
-import { cn } from '@/lib/utils';
-import * as AvatarPrimitive from '@rn-primitives/avatar';
+import { cn } from '@/lib/utils'
+import * as AvatarPrimitive from '@rn-primitives/avatar'
 
 function Avatar({
   className,
@@ -7,17 +7,25 @@ function Avatar({
 }: React.ComponentProps<typeof AvatarPrimitive.Root>) {
   return (
     <AvatarPrimitive.Root
-      className={cn('relative flex size-8 shrink-0 overflow-hidden rounded-full', className)}
+      className={cn(
+        'relative flex size-8 shrink-0 overflow-hidden rounded-full',
+        className,
+      )}
       {...props}
     />
-  );
+  )
 }
 
 function AvatarImage({
   className,
   ...props
 }: React.ComponentProps<typeof AvatarPrimitive.Image>) {
-  return <AvatarPrimitive.Image className={cn('aspect-square size-full', className)} {...props} />;
+  return (
+    <AvatarPrimitive.Image
+      className={cn('aspect-square size-full', className)}
+      {...props}
+    />
+  )
 }
 
 function AvatarFallback({
@@ -27,12 +35,12 @@ function AvatarFallback({
   return (
     <AvatarPrimitive.Fallback
       className={cn(
-        'bg-muted flex size-full flex-row items-center justify-center rounded-full',
-        className
+        'flex size-full flex-row items-center justify-center rounded-full bg-muted',
+        className,
       )}
       {...props}
     />
-  );
+  )
 }
 
-export { Avatar, AvatarFallback, AvatarImage };
+export { Avatar, AvatarFallback, AvatarImage }
