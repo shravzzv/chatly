762453127c3d3763ad4b15e85d89f0f0c519ea10
Sai Shravan Vadla <94429984+shravzzv@users.noreply.github.com>
2026-06-05 20:11:59 +0530

    feat: update android download link and QR code (#99)
    * chore: add local packages to root `package.json`
    * feat: update android download QR and add app download link

--- apps/web/app/(public)/download/page.tsx
+++ apps/web/app/(public)/download/page.tsx
@@ -4,6 +4,7 @@ import { Button } from '@/components/ui/button'
 import { Spinner } from '@/components/ui/spinner'
 import { AlertCircle, CheckCircle, Download } from 'lucide-react'
 import Image from 'next/image'
+import Link from 'next/link'
 import { useEffect, useRef, useState } from 'react'
 import qr from '../../../public/android-download-qr.png'
 
@@ -106,8 +107,22 @@ export default function Page() {
 
       <div className='space-y-2'>
         <h2 className='text-xl font-bold'>Android</h2>
-        <p>Scan this QR code to install on android:</p>
-        <Image src={qr} alt='android download qr code' className='size-48' />
+        <p>
+          Click this{' '}
+          <Link
+            href='https://github.com/shravzzv/chatly/releases/download/v1.1.0/chatly.apk'
+            download
+            className='underline underline-offset-2'
+          >
+            link
+          </Link>{' '}
+          or scan this QR to install the android app.
+        </p>
+        <Image
+          src={qr}
+          alt='android download qr code'
+          className='size-48 rounded-sm shadow-lg'
+        />
       </div>
     </div>
   )
